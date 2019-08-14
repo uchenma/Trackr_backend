a8c98f67b9ded0510ca04a367635539575b79760
uchenma <uchenm@seas.upenn.edu>
2019-08-14 13:45:54 -0400

    initial commit and mongoDB connected

---
+++ server.js
@@ -0,0 +1,27 @@
+
+const bodyParser = require("body-parser");
+const path = require("path");
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+const server = require("http").Server(app);
+
+// static
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(express.static(path.join(__dirname, "build")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// mongo stuff
+const REQUIRED_ENVS = ["MONGODB_URI"];
+REQUIRED_ENVS.forEach(function(el) {
+  if (!process.env[el]) throw new Error("Missing required env var " + el);
+});
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("open", () => console.log(`Connected to MongoDB!`));
+mongoose.connection.on("error", function(err) {
+  console.log("Mongoose default connection error: " + err);
+});
